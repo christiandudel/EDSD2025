4e1e27e9b7ec69321cc0fac278d4e826100e2b6d
Dudel <dudel@demogr.mpg.de>
2025-09-02 14:32:24 +0200

    Updated code for second session

--- R Code/02-descriptive.R
+++ R Code/02-descriptive.R
@@ -1,6 +1,6 @@
-### Second session: Descriptive statistics ##########################
+### Second session: Descriptive statistics #####################################
 
-### Assignment operator #######################################################
+### Assignment operator ########################################################
 
   x <- 5
   x
@@ -30,93 +30,8 @@
   x
 
   
-### Example data ####################################################
-
-  # Package coming with R
-  library(MASS)
+### Example data ###############################################################
   
-  # Load data coming with MASS package
-  data(birthwt)
-  
-  # Get help on data
-  help(birthwt)
-
-
-### Viewing data ####################################################
-
-  # View data in console
-  birthwt
-  
-  # Class of object
-  class(birthwt) 
-  
-  # Overview of data
-  head(birthwt)
-  tail(birthwt)
-  view(birthwt) # Or click in R Studio under "Environment" tab
-  
-  # Variable names
-  names(birthwt)
-  
-  # Size of data set
-  dim(birthwt)
-
-
-### View parts of data ##############################################
-
-  # Variable names
-  birthwt$bwt # Vector
-  birthwt["bwt"] # Data frame
-
-
-### Descriptive statistics ##########################################
-
-  # Mean
-  mean(birthwt$bwt)
-  mean(birthwt$age)
-  
-  # Median
-  median(birthwt$bwt)
-  median(birthwt$age)
-  
-  # Standard deviation
-  sd(birthwt$bwt)
-  sd(birthwt$age)
-  
-  # Variance
-  var(birthwt$bwt)
-  var(birthwt$age)
-  
-  # Quantiles
-  quantile(birthwt$bwt)
-  quantile(birthwt$age)
-  
-  # Summary
-  summary(birthwt$bwt)
-  summary(birthwt)
-
-
-### Tables ##########################################################
-
-  # Simple tables 
-  table(birthwt$age)
-  table(birthwt$low)
-  
-  # Cross tabulations
-  table(birthwt$low,birthwt$smoke)
-  table(birthwt$low,birthwt$smoke,dnn=c("Low","Smoke"))
-  
-  # Tables can be stored in objects
-  tab <- table(birthwt$low,birthwt$smoke,dnn=c("Low","Smoke"))
-  
-  # Proportions
-  prop.table(tab)
-  prop.table(tab,1)
-  prop.table(tab,2)
-
-
-### Another example: German Socio-Economic Panel (GSOEP) ############
-
   # Source: 
   # https://www.diw.de/documents/dokumentenarchiv/17/diw_01.c.412698.de/soep_lebensz_en.zip
   # https://www.diw.de/en/diw_02.c.222838.en/soep_in_the_college_classroom.html
@@ -127,36 +42,101 @@
   library(foreign)
   
   # Load data
-  soep <- read.dta(file="D:/soep_lebensz_en.dta",
+  soep <- read.dta(file="U:/soep_lebensz_en.dta",
                    convert.factors=F)
   
-  # Longitudinal data
+  
+### Viewing data ###############################################################  
+  
+  # View data in console
+  soep
+  
+  # Class of object
+  class(soep) 
+  
+  # Overview of data
   head(soep)
+  tail(soep)
+  View(soep) # Or click in R Studio under "Environment" tab
+  
+  # Variable names
+  names(soep)
+  
+  # Size of data set
+  dim(soep)
+  
+  
+### View parts of data #########################################################
+  
+  # Variable names
+  soep$id # Vector
+  soep["id"] # Data frame  
+  
+  
+### Restricting data ###########################################################
   
   # Restrict data to last year
   soep <- subset(x=soep,
                  subset=year==2004)
   
-  # Mean satisfaction
+  
+### Descriptive statistics #####################################################
+  
+  # Mean
   mean(soep$satisf_org)
-  
-  # Number of kids
-  summary(soep$no_kids)
   mean(soep$no_kids)
+  mean(soep$no_kids,na.rm=T)
   
-  # Remove missing values (row-wise deletion)
-  soep <- na.omit(soep)
+  # Median
+  median(soep$satisf_org)
+  median(soep$no_kids,na.rm=T)
   
-  # Conditional statistics
+  # Standard deviation
+  sd(soep$satisf_org)
+  sd(soep$no_kids,na.rm=T)
+  
+  
+  # Variance
+  var(soep$satisf_org)
+  var(soep$no_kids,na.rm=T)
+  
+  # Quantiles
+  quantile(soep$satisf_org)
+  quantile(soep$no_kids,na.rm=T)
+  
+  # Summary
+  summary(soep$satisf_org)
+  summary(soep$no_kids)
+  summary(soep)
+  
+  
+### Tables #####################################################################
+  
+  # Simple tables 
+  table(soep$no_kids)
+  table(soep$sex)
+  
+  # Cross tabulations
+  table(soep$no_kids,soep$sex)
+  table(soep$no_kids,soep$sex,dnn=c("Kids","Sex"))
+  
+  # Tables can be stored in objects
+  tab <- table(soep$no_kids,soep$sex,dnn=c("Kids","Sex"))
+  
+  # Proportions
+  prop.table(tab)
+  prop.table(tab,1)
+  prop.table(tab,2)
+  
+  
+### Condiitonal statistics #####################################################
+  
   by(data=soep$satisf_org,
      INDICES=soep$no_kids,
      FUN=mean)
   
-  # Alternative
-  mean(soep$satisf_org[soep$no_kids==1])
-
-
-### Reading another data type: CSV ##################################
+  
+### Reading another data type: CSV #############################################
 
   # Data source and description:
   # https://www.google.com/covid19/mobility/
